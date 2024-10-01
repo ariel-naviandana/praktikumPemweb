96e1e5fba5e912ab80eeac0ef49cd51b1b2c5234
Ariel2711 <arielnaviandanaputra@gmail.com>
2024-10-01 15:15:15 +0700

    add modul 5

---
+++ modul5/soal3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="proses02.php" method="post" name="input">
+        Nama Anda: <input type="text" name="nama">
+        <br>
+        <input type="submit" name="input" value="Input">
+    </form>
+</body>
+</html>
